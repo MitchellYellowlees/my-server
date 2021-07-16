89ae76acd6080a41034b49b3c1bb3070eb76b8a0
Mitchell <miy456@mail.usask.ca>
2021-07-16 17:05:50 -0600

    attempting non https server

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
-
-
 
 //requiring dotenv
 const dotenv = require('dotenv')
@@ -38,12 +33,7 @@ const { server } = require('sinon')
 app.use('/entries', entryRouter)
 app.use('/users', userRouter)
 
-var options = {
-    key: fs.readFileSync('key.pem'),
-    ca: fs.readFileSync('certificate.pem')
-}
-
-https.createServer(options, app).listen(process.env.PORT || PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
 
