a9f810978590615388adc4d4b060fd8e3d0c3900
Mitchell <miy456@mail.usask.ca>
2021-07-16 18:29:02 -0600

    fix attempts

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -32,7 +32,7 @@ const { server } = require('sinon')
 app.use('/entries', entryRouter)
 app.use('/users', userRouter)
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
 
