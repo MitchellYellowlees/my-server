cca6f57a342bcc0d940189339fc36deb9cdf7223
Mitchell <miy456@mail.usask.ca>
2021-07-17 15:58:08 -0600

    replaced mongoose with mongodb

--- my_functions/users.js
+++ my_functions/users.js
@@ -1,40 +1,41 @@
 const User = require('../models/user.model')
-const cors = require('cors')
-const express = require('express')
-const mongoose = require("mongoose")
 const dotenv = require('dotenv')
 
+dotenv.config()
+
+const MongoClient = require("mongodb").MongoClient;
+let cachedDb = null;
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  const client = await MongoClient.connect(process.env.CONNECTIONSTRING);
+  const db = await client.db('Cluster0');
+  cachedDb = db;
+  return db
+}
 
 exports.handler = async function (event, context) {
-    dotenv.config()
-    const app = express()
-    const port = process.env.PORT || 5000
+    
+    context.callbackWaitsForEmptyEventLoop = false;
 
-    app.use(cors())
-    app.use(express.json())
-
-    mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
-
+    const db = await connectToDatabase();
 
     const commandArray = event.path.split("/")
     let userEmail = commandArray.pop()
     let command = commandArray.pop()
 
     
-
     
     if (command === "get-by-email") {
-        app.get((userEmail, res) => {
-            User.findOne({email: userEmail})
-            .then((user) => res.json({message: "Got user with email that was passed in", response: user,}))
-            .catch((err) => res.status(400).json({message:"Error: could not get user with given email", response:err,}))
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify('Do i get past this code'),
-            };
-        })
-        
+        const result = await db.collection("user").findOne({email: userEmail});
+        const response = {
+            statusCode: 200,
+            body:JSON.stringify(result),
+        };
         return response;
+
+        
         //GET user via email
     }
     else if (command === "entries-with-email"){
