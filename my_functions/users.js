ce4860e52c7faef3f3488a16b2075be3988a2aa4
Mitchell <miy456@mail.usask.ca>
2021-07-17 17:19:17 -0600

    creating user

--- my_functions/users.js
+++ my_functions/users.js
@@ -49,16 +49,12 @@ exports.handler = async function (event, context) {
         //GET user entries via email
     }
     else if (userEmail === "create-user") {
-        await db.collection("users").post((req, res) => {
-            const email = req.body.email
-
-    const newUser = new User({
-        email,
-    })
-
-    newUser
-    .save()
-        })
+        await db.collection("users").insertOne({email:"fake@fake.net"})
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify("hello there"),
+        };
+        return response;
         //POST new user
     }
     
