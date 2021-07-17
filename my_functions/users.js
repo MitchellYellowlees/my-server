dba5b2446616f213444552c28c00b85830f0f7bd
Mitchell <miy456@mail.usask.ca>
2021-07-17 17:39:05 -0600

    test query

--- my_functions/users.js
+++ my_functions/users.js
@@ -49,10 +49,10 @@ exports.handler = async function (event, context) {
         //GET user entries via email
     }
     else if (userEmail === "create-user") {
-        await db.collection("users").insertOne({email:event["email"]})
+        await db.collection("users").insertOne({email:"testmail@test.test"})
         const response = {
             statusCode: 200,
-            body: JSON.stringify(event["email"]),
+            body: JSON.stringify(event),
         };
         return response;
         //POST new user
