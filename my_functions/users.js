8a17aaef5781cc3ba3e0a0c4bdd3a9597216a70c
Mitchell <miy456@mail.usask.ca>
2021-07-17 22:18:29 -0600

    curly braces in header

--- my_functions/users.js
+++ my_functions/users.js
@@ -65,7 +65,7 @@ exports.handler = async function (event, context) {
             statusCode: 200,
             headers: {
                 'Access-Control-Allow-Origin': '*',
-                
+                'Access-Control-Allow-Methods': {GET, POST, PUT, DELETE,OPTIONS},
                 'Access-Control-Allow-Credentials': true,
               },
             body: JSON.stringify("New user added successfully"),
